3631c75638c1f8fdbf58f0fb1d83a92f32e943bd
Joseph Zhao <joseph.zhao@xing.net.au>
2015-11-14 12:10:04 +1100

    Init project

---
+++ templates/html--minisite.tpl.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @file
+ * Minisite default template.
+ *
+ * Variables:
+ * - $minisite: Raw html of minisite.
+ *
+ * @see minisite_theme_registry_alter()
+ * @see minisite_preprocess_html()
+ *
+ * * @ingroup themeable
+ */
+?>
+
+<?php print $minisite; ?>
