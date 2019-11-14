dd5277737fea550a6b82c487f8731746a81fa1df
Alex Skrypnyk <alex.designworks@gmail.com>
2019-11-14 20:10:01 +1100

    added screenshot

--- tests/src/Functional/MinisiteTestBase.php
+++ tests/src/Functional/MinisiteTestBase.php
@@ -2,11 +2,9 @@
 
 namespace Drupal\Tests\minisite\Functional;
 
-use Drupal\field\Entity\FieldStorageConfig;
-use Drupal\field\Entity\FieldConfig;
-use Drupal\file\FileInterface;
-use Drupal\Tests\BrowserTestBase;
+use Drupal\Component\Utility\Html;
 use Drupal\file\Entity\File;
+use Drupal\Tests\BrowserTestBase;
 use Drupal\Tests\TestFileCreationTrait;
 
 /**
@@ -59,207 +57,42 @@ abstract class MinisiteTestBase extends BrowserTestBase {
 
     return File::create((array) $file);
   }
-//
-//  /**
-//   * Retrieves the fid of the last inserted file.
-//   */
-//  public function getLastFileId() {
-//    return (int) \Drupal::entityQueryAggregate('file')->aggregate('fid', 'max')->execute()[0]['fid_max'];
-//  }
-//
-//  /**
-//   * Updates an existing minisite field with new settings.
-//   */
-//  public function updateMinisite($name, $type_name, $field_settings = [], $widget_settings = []) {
-//    $field = FieldConfig::loadByName('node', $type_name, $name);
-//    $field->setSettings(array_merge($field->getSettings(), $field_settings));
-//    $field->save();
-//
-//    entity_get_form_display('node', $type_name, 'default')
-//      ->setComponent($name, [
-//        'settings' => $widget_settings,
-//      ])
-//      ->save();
-//  }
-//
-//  /**
-//   * Uploads a file to a node.
-//   *
-//   * @param \Drupal\file\FileInterface $file
-//   *   The File to be uploaded.
-//   * @param string $field_name
-//   *   The name of the field on which the files should be saved.
-//   * @param $nid_or_type
-//   *   A numeric node id to upload files to an existing node, or a string
-//   *   indicating the desired bundle for a new node.
-//   * @param bool $new_revision
-//   *   The revision number.
-//   * @param array $extras
-//   *   Additional values when a new node is created.
-//   *
-//   * @return int
-//   *   The node id.
-//   */
-//  public function uploadNodeFile(FileInterface $file, $field_name, $nid_or_type, $new_revision = TRUE, array $extras = []) {
-//    return $this->uploadNodeFiles([$file], $field_name, $nid_or_type, $new_revision, $extras);
-//  }
-//
-//  /**
-//   * Uploads multiple files to a node.
-//   *
-//   * @param \Drupal\file\FileInterface[] $files
-//   *   The files to be uploaded.
-//   * @param string $field_name
-//   *   The name of the field on which the files should be saved.
-//   * @param $nid_or_type
-//   *   A numeric node id to upload files to an existing node, or a string
-//   *   indicating the desired bundle for a new node.
-//   * @param bool $new_revision
-//   *   The revision number.
-//   * @param array $extras
-//   *   Additional values when a new node is created.
-//   *
-//   * @return int
-//   *   The node id.
-//   */
-//  public function uploadNodeFiles(array $files, $field_name, $nid_or_type, $new_revision = TRUE, array $extras = []) {
-//    $edit = [
-//      'title[0][value]' => $this->randomMachineName(),
-//      'revision' => (string) (int) $new_revision,
-//    ];
-//
-//    $node_storage = $this->container->get('entity.manager')->getStorage('node');
-//    if (is_numeric($nid_or_type)) {
-//      $nid = $nid_or_type;
-//      $node_storage->resetCache([$nid]);
-//      $node = $node_storage->load($nid);
-//    }
-//    else {
-//      // Add a new node.
-//      $extras['type'] = $nid_or_type;
-//      $node = $this->drupalCreateNode($extras);
-//      $nid = $node->id();
-//      // Save at least one revision to better simulate a real site.
-//      $node->setNewRevision();
-//      $node->save();
-//      $node_storage->resetCache([$nid]);
-//      $node = $node_storage->load($nid);
-//      $this->assertNotEqual($nid, $node->getRevisionId(), 'Node revision exists.');
-//    }
-//    $this->drupalGet("node/$nid/edit");
-//    $page = $this->getSession()->getPage();
-//
-//    // Attach files to the node.
-//    $field_storage = FieldStorageConfig::loadByName('node', $field_name);
-//    // File input name depends on number of files already uploaded.
-//    $field_num = count($node->{$field_name});
-//    foreach ($files as $i => $file) {
-//      $delta = $field_num + $i;
-//      $file_path = $this->container->get('file_system')->realpath($file->getFileUri());
-//      $name = 'files[' . $field_name . '_' . $delta . ']';
-//      if ($field_storage->getCardinality() != 1) {
-//        $name .= '[]';
-//      }
-//      if (count($files) == 1) {
-//        $edit[$name] = $file_path;
-//      }
-//      else {
-//        $page->attachFileToField($name, $file_path);
-//        $this->drupalPostForm(NULL, [], t('Upload'));
-//      }
-//    }
-//
-//    $this->drupalPostForm(NULL, $edit, t('Save'));
-//
-//    return $nid;
-//  }
-//
-//  /**
-//   * Removes a file from a node.
-//   *
-//   * Note that if replacing a file, it must first be removed then added again.
-//   */
-//  public function removeNodeFile($nid, $new_revision = TRUE) {
-//    $edit = [
-//      'revision' => (string) (int) $new_revision,
-//    ];
-//
-//    $this->drupalPostForm('node/' . $nid . '/edit', [], t('Remove'));
-//    $this->drupalPostForm(NULL, $edit, t('Save'));
-//  }
-//
-//  /**
-//   * Replaces a file within a node.
-//   */
-//  public function replaceNodeFile($file, $field_name, $nid, $new_revision = TRUE) {
-//    $edit = [
-//      'files[' . $field_name . '_0]' => \Drupal::service('file_system')->realpath($file->getFileUri()),
-//      'revision' => (string) (int) $new_revision,
-//    ];
-//
-//    $this->drupalPostForm('node/' . $nid . '/edit', [], t('Remove'));
-//    $this->drupalPostForm(NULL, $edit, t('Save'));
-//  }
-//
-//  /**
-//   * Asserts that a file exists physically on disk.
-//   *
-//   * Overrides PHPUnit\Framework\Assert::assertFileExists() to also work with
-//   * file entities.
-//   *
-//   * @param \Drupal\File\FileInterface|string $file
-//   *   Either the file entity or the file URI.
-//   * @param string $message
-//   *   (optional) A message to display with the assertion.
-//   */
-//  public static function assertFileExists($file, $message = NULL) {
-//    $message = isset($message) ? $message : format_string('File %file exists on the disk.', ['%file' => $file->getFileUri()]);
-//    $filename = $file instanceof FileInterface ? $file->getFileUri() : $file;
-//    parent::assertFileExists($filename, $message);
-//  }
-//
-//  /**
-//   * Asserts that a file exists in the database.
-//   */
-//  public function assertFileEntryExists($file, $message = NULL) {
-//    $this->container->get('entity.manager')->getStorage('file')->resetCache();
-//    $db_file = File::load($file->id());
-//    $message = isset($message) ? $message : format_string('File %file exists in database at the correct path.', ['%file' => $file->getFileUri()]);
-//    $this->assertEqual($db_file->getFileUri(), $file->getFileUri(), $message);
-//  }
-//
-//  /**
-//   * Asserts that a file does not exist on disk.
-//   *
-//   * Overrides PHPUnit\Framework\Assert::assertFileExists() to also work with
-//   * file entities.
-//   *
-//   * @param \Drupal\File\FileInterface|string $file
-//   *   Either the file entity or the file URI.
-//   * @param string $message
-//   *   (optional) A message to display with the assertion.
-//   */
-//  public static function assertFileNotExists($file, $message = NULL) {
-//    $message = isset($message) ? $message : format_string('File %file exists on the disk.', ['%file' => $file->getFileUri()]);
-//    $filename = $file instanceof FileInterface ? $file->getFileUri() : $file;
-//    parent::assertFileNotExists($filename, $message);
-//  }
-//
-//  /**
-//   * Asserts that a file does not exist in the database.
-//   */
-//  public function assertFileEntryNotExists($file, $message) {
-//    $this->container->get('entity.manager')->getStorage('file')->resetCache();
-//    $message = isset($message) ? $message : format_string('File %file exists in database at the correct path.', ['%file' => $file->getFileUri()]);
-//    $this->assertFalse(File::load($file->id()), $message);
-//  }
-//
-//  /**
-//   * Asserts that a file's status is set to permanent in the database.
-//   */
-//  public function assertFileIsPermanent(FileInterface $file, $message = NULL) {
-//    $message = isset($message) ? $message : format_string('File %file is permanent.', ['%file' => $file->getFileUri()]);
-//    $this->assertTrue($file->isPermanent(), $message);
-//  }
+
+  /**
+   * Captures and saves a screenshot.
+   *
+   * The result of calling this function will be triggering fail in order to
+   * output the URL to the generated screenshot. This is due to result printer
+   * not allowing print output from within the test.
+   *
+   * @todo: Find a better way to add output to the result printer.
+   */
+  protected function screenshot() {
+    $base_directory = '/sites/simpletest/browser_output';
+
+    $directory = DRUPAL_ROOT . $base_directory;
+
+    // Ensure directory exists.
+    if (!is_dir($directory)) {
+      mkdir($directory, 0777, TRUE);
+    }
+
+    $current_url = substr(Html::cleanCssIdentifier($this->getSession()->getCurrentUrl()), 100);
+
+    $filename = uniqid() . '_' . $current_url . '.html';
+    $full_filename = file_create_filename($filename, $directory);
+
+    $screenshot = $this->getSession()->getPage()->getContent();
+    file_put_contents($full_filename, $screenshot);
+
+    $url = $GLOBALS['base_url'] . $base_directory . '/' . $filename;
+
+    $this->fail($url);
+  }
+
+
+  protected function createFixtureArchive($filename, $structure) {
+
+  }
 
 }
