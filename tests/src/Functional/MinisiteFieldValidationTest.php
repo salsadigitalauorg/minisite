e586eaacd5c470cf92f6621633cb00cde01291f9
Alex Skrypnyk <alex@integratedexperts.com>
2019-11-25 20:33:42 +1100

    Locked cardinality to 1 + tests.

--- tests/src/Functional/MinisiteFieldValidationTest.php
+++ tests/src/Functional/MinisiteFieldValidationTest.php
@@ -3,7 +3,6 @@
 namespace Drupal\Tests\minisite\Functional;
 
 use Drupal\Component\Render\FormattableMarkup;
-use Drupal\Core\Field\FieldStorageDefinitionInterface;
 use Drupal\field\Entity\FieldConfig;
 use Drupal\file\Entity\File;
 use Drupal\Tests\field_ui\Traits\FieldUiTestTrait;
@@ -37,7 +36,7 @@ class MinisiteFieldValidationTest extends MinisiteTestBase {
   public function testValidateRequired() {
     $node_storage = $this->container->get('entity.manager')->getStorage('node');
     $field_name = strtolower($this->randomMachineName());
-    $storage = $this->createMinisiteField($field_name, 'node', $this->contentType, [], ['required' => '1']);
+    $this->createMinisiteField($field_name, 'node', $this->contentType, [], ['required' => '1']);
     $field = FieldConfig::loadByName('node', $this->contentType, $field_name);
 
     // Try to post a new node without uploading a file.
@@ -61,24 +60,6 @@ class MinisiteFieldValidationTest extends MinisiteTestBase {
     $node_file = File::load($node->{$field_name}->target_id);
     $this->assertFileExists($node_file, 'File exists after uploading to the required field.');
     $this->assertFileEntryExists($node_file, 'File entry exists after uploading to the required field.');
-
-    // Try again with a multiple value field.
-    $storage->delete();
-    $this->createMinisiteField($field_name, 'node', $this->contentType, ['cardinality' => FieldStorageDefinitionInterface::CARDINALITY_UNLIMITED], ['required' => '1']);
-
-    // Try to post a new node without uploading a file in the multivalue field.
-    $edit = [];
-    $edit['title[0][value]'] = $this->randomMachineName();
-    $this->drupalPostForm('node/add/' . $this->contentType, $edit, $this->t('Save'));
-    $this->assertRaw($this->t('@title field is required.', ['@title' => $field->getLabel()]));
-
-    // Create a new node with the uploaded file into the multivalue field.
-    $nid = $this->uploadNodeFile($test_file, $field_name, $this->contentType);
-    $node_storage->resetCache([$nid]);
-    $node = $node_storage->load($nid);
-    $node_file = File::load($node->{$field_name}->target_id);
-    $this->assertFileExists($node_file, 'File exists after uploading to the required multiple value field.');
-    $this->assertFileEntryExists($node_file, 'File entry exists after uploading to the required multiple value field.');
   }
 
   /**
