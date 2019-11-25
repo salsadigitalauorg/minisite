757846cc90dc1f5ecf0c955a613fce81ec70eb2c
Alex Skrypnyk <alex@integratedexperts.com>
2019-11-25 18:31:46 +1100

    Added support for root-level directories exclusions during validation + tests.

--- tests/src/Unit/MinisiteValidatorTest.php
+++ tests/src/Unit/MinisiteValidatorTest.php
@@ -6,6 +6,7 @@ use Drupal\minisite\Exception\InvalidContentArchiveException;
 use Drupal\minisite\Exception\InvalidExtensionValidatorException;
 use Drupal\minisite\MinisiteAsset;
 use Drupal\minisite\MinisiteValidator;
+use Drupal\Tests\minisite\Traits\MockHelperTrait;
 use Drupal\Tests\UnitTestCase;
 
 /**
@@ -18,6 +19,8 @@ use Drupal\Tests\UnitTestCase;
  * @package Drupal\testmode\Tests
  */
 class MinisiteValidatorTest extends UnitTestCase {
+
+  use MockHelperTrait;
 
   /**
    * Test validateFileExtension() method.
@@ -32,7 +35,7 @@ class MinisiteValidatorTest extends UnitTestCase {
   }
 
   /**
-   * Data provider for testValidateFileExtension.
+   * Data provider for testValidateFileExtension().
    */
   public function dataProviderValidateFileExtension() {
     return [
@@ -40,6 +43,267 @@ class MinisiteValidatorTest extends UnitTestCase {
       ['file.txt', ['ext', 'ext2']],
       ['file', ['ext', 'ext2']],
       ['file.ext.txt', ['ext', 'ext2']],
+    ];
+  }
+
+  /**
+   * Test normaliseExtensions() method.
+   *
+   * @dataProvider dataProviderNormaliseExtensions
+   * @covers \Drupal\minisite\MinisiteValidator::normaliseExtensions
+   */
+  public function testNormaliseExtensions($extensions, $expected) {
+    $actual = MinisiteValidator::normaliseExtensions($extensions);
+    $this->assertEquals($expected, $actual);
+  }
+
+  /**
+   * Data provider for testNormaliseExtensions().
+   */
+  public function dataProviderNormaliseExtensions() {
+    return [
+      [[], []],
+      ['', []],
+      [' ', []],
+      [', ', []],
+      [' , ', []],
+      ['  , ', []],
+      ['  ,', []],
+
+      ['a b c', ['a', 'b', 'c']],
+      ['a,b,c', ['a', 'b', 'c']],
+      ['a, b, c', ['a', 'b', 'c']],
+      ['a, b , c', ['a', 'b', 'c']],
+      ['a, b c', ['a', 'b', 'c']],
+      ['   a,    b   c', ['a', 'b', 'c']],
+    ];
+  }
+
+  /**
+   * Test filesToTree() method.
+   *
+   * @dataProvider dataProviderFilesToTree
+   * @covers \Drupal\minisite\MinisiteValidator::filesToTree
+   */
+  public function testFilesToTree($files, $expected, $expectException = FALSE) {
+    if ($expectException) {
+      $this->expectException(\RuntimeException::class);
+      $this->expectExceptionMessage('Invalid file list provided');
+    }
+    $actual = $this->callProtectedMethod(MinisiteValidator::class, 'filesToTree', [$files]);
+    $this->assertEquals($expected, $actual);
+  }
+
+  /**
+   * Data provider for testFilesToTree().
+   */
+  public function dataProviderFilesToTree() {
+    return [
+      // Root files.
+      [
+        [
+          'file.txt',
+        ],
+        [
+          'file.txt' => 'file.txt',
+        ],
+      ],
+
+      [
+        [
+          'file1.txt',
+          'file2.txt',
+        ],
+        [
+          'file1.txt' => 'file1.txt',
+          'file2.txt' => 'file2.txt',
+        ],
+      ],
+
+      // Simple dirs.
+      [
+        [
+          'dir1/',
+        ],
+        [
+          'dir1' => [
+            '.' => 'dir1/',
+          ],
+        ],
+      ],
+      [
+        [
+          'dir1/',
+          'dir2/',
+        ],
+        [
+          'dir1' => [
+            '.' => 'dir1/',
+          ],
+          'dir2' => [
+            '.' => 'dir2/',
+          ],
+        ],
+      ],
+
+      // Dirs with files.
+      [
+        [
+          'dir1/file1.txt',
+          'dir2/file2.txt',
+        ],
+        [
+          'dir1' => [
+            '.' => 'dir1/',
+            'file1.txt' => 'dir1/file1.txt',
+          ],
+          'dir2' => [
+            '.' => 'dir2/',
+            'file2.txt' => 'dir2/file2.txt',
+          ],
+        ],
+      ],
+      [
+        [
+          'dir1/dir11/dir111/',
+        ],
+        [
+          'dir1' => [
+            '.' => 'dir1/',
+            'dir11' => [
+              '.' => 'dir1/dir11/',
+              'dir111' => [
+                '.' => 'dir1/dir11/dir111/',
+              ],
+            ],
+          ],
+        ],
+      ],
+
+      [
+        [
+          'dir1/file11.txt',
+          'dir2/file21.txt',
+          'dir2/file22.txt',
+          'dir3/',
+          'dir4/dir41/',
+        ],
+        [
+          'dir1' => [
+            '.' => 'dir1/',
+            'file11.txt' => 'dir1/file11.txt',
+          ],
+          'dir2' => [
+            '.' => 'dir2/',
+            'file21.txt' => 'dir2/file21.txt',
+            'file22.txt' => 'dir2/file22.txt',
+          ],
+          'dir3' => [
+            '.' => 'dir3/',
+          ],
+          'dir4' => [
+            '.' => 'dir4/',
+            'dir41' => [
+              '.' => 'dir4/dir41/',
+            ],
+          ],
+        ],
+      ],
+
+      // Mixed dirs, files, order.
+      [
+        [
+          'dir1/',
+          'dir1/dir11/dir111/',
+          'dir1/dir11/',
+          'dir1/dir11/dir111/file111.txt',
+          'dir1/dir11/dir111/file112.txt',
+          'file1.txt',
+          'file2.txt',
+          'dir2/file21.txt',
+          'dir2/dir21/file211.txt',
+          'dir2/file22.txt',
+          'dir2/dir21/file212.txt',
+          'dir2/file23.txt',
+        ],
+        [
+          'file1.txt' => 'file1.txt',
+          'file2.txt' => 'file2.txt',
+          'dir1' => [
+            '.' => 'dir1/',
+            'dir11' => [
+              '.' => 'dir1/dir11/',
+              'dir111' => [
+                '.' => 'dir1/dir11/dir111/',
+                'file111.txt' => 'dir1/dir11/dir111/file111.txt',
+                'file112.txt' => 'dir1/dir11/dir111/file112.txt',
+              ],
+            ],
+          ],
+          'dir2' => [
+            '.' => 'dir2/',
+            'dir21' => [
+              '.' => 'dir2/dir21/',
+              'file211.txt' => 'dir2/dir21/file211.txt',
+              'file212.txt' => 'dir2/dir21/file212.txt',
+            ],
+            'file21.txt' => 'dir2/file21.txt',
+            'file22.txt' => 'dir2/file22.txt',
+            'file23.txt' => 'dir2/file23.txt',
+          ],
+        ],
+      ],
+
+      // Edge case - dirs provided as files and then as dirs.
+      [
+        [
+          'dir1',
+          'dir1/file1.txt',
+        ],
+        [],
+        TRUE,
+      ],
+
+      [
+        [
+          'dir1/file1.txt',
+          'dir1',
+        ],
+        [],
+        TRUE,
+      ],
+
+      [
+        [
+          'dir1/dir11/dir111/file111.txt',
+          'dir1/dir11/dir111',
+        ],
+        [],
+        TRUE,
+      ],
+
+      // Repeating files.
+      [
+        [
+          'file1.txt',
+          'file1.txt',
+        ],
+        [
+          'file1.txt' => 'file1.txt',
+        ],
+      ],
+      [
+        [
+          'dir1/file1.txt',
+          'dir1/file1.txt',
+        ],
+        [
+          'dir1' => [
+            '.' => 'dir1/',
+            'file1.txt' => 'dir1/file1.txt',
+          ],
+        ],
+      ],
     ];
   }
 
@@ -61,56 +325,73 @@ class MinisiteValidatorTest extends UnitTestCase {
   public function dataProviderValidateFiles() {
     return [
       [
-        ['file.txt'], ['ext'], 'A single top level directory is expected.',
+        [
+          'file.txt',
+        ],
+        ['ext'],
+        'A single top level directory is expected.',
       ],
       [
-        ['file.txt', 'dir1/file.txt'], ['ext'], 'A single top level directory is expected.',
+        [
+          'file.txt',
+          'dir1/file.txt',
+        ],
+        ['ext'],
+        'A single top level directory is expected.',
       ],
       [
-        ['dir1/file.txt'], ['ext'], sprintf('Missing required %s file.', MinisiteAsset::INDEX_FILE),
+        [
+          'dir1/',
+          'dir2/',
+          'dir1/file.txt',
+        ],
+        ['ext'],
+        'A single top level directory is expected.',
       ],
       [
-        ['dir1/', 'dir1/file.txt'], ['ext'], sprintf('Missing required %s file.', MinisiteAsset::INDEX_FILE),
+        [
+          'dir1/file.txt',
+        ],
+        ['ext'],
+        sprintf('Missing required %s file.', MinisiteAsset::INDEX_FILE),
       ],
       [
-        ['dir1/' . MinisiteAsset::INDEX_FILE, 'dir1/file.txt'], ['html', 'ext'], 'Archive has invalid content: File dir1/file.txt has invalid extension.',
+        [
+          'dir1/',
+          'dir1/file.txt',
+        ],
+        ['ext'],
+        sprintf('Missing required %s file.', MinisiteAsset::INDEX_FILE),
       ],
       [
-        ['dir1/' . MinisiteAsset::INDEX_FILE, 'dir1/file.txt', 'dir1/file2.txt'], ['html', 'ext'], 'Archive has invalid content: File dir1/file.txt has invalid extension.' . PHP_EOL . 'File dir1/file2.txt has invalid extension.',
+        [
+          'dir1/' . MinisiteAsset::INDEX_FILE,
+          'dir1/file.txt',
+        ],
+        ['html', 'ext'],
+        'Archive has invalid content: File dir1/file.txt has invalid extension.',
       ],
-    ];
-  }
+      [
+        [
+          'dir1/' . MinisiteAsset::INDEX_FILE,
+          'dir1/file.txt', 'dir1/file2.txt',
+        ],
+        ['html', 'ext'],
+        'Archive has invalid content: File dir1/file.txt has invalid extension.' . PHP_EOL . 'File dir1/file2.txt has invalid extension.',
+      ],
 
-  /**
-   * Test normaliseExtensions() method.
-   *
-   * @dataProvider dataProviderNormaliseExtensions
-   * @covers \Drupal\minisite\MinisiteValidator::normaliseExtensions
-   */
-  public function testNormaliseExtensions($extensions, $expected) {
-    $actual = MinisiteValidator::normaliseExtensions($extensions);
-    $this->assertEquals($expected, $actual);
-  }
-
-  /**
-   * Data provider for testNormaliseExtensions.
-   */
-  public function dataProviderNormaliseExtensions() {
-    return [
-      [[], []],
-      ['', []],
-      [' ', []],
-      [', ', []],
-      [' , ', []],
-      ['  , ', []],
-      ['  ,', []],
-
-      ['a b c', ['a', 'b', 'c']],
-      ['a,b,c', ['a', 'b', 'c']],
-      ['a, b, c', ['a', 'b', 'c']],
-      ['a, b , c', ['a', 'b', 'c']],
-      ['a, b c', ['a', 'b', 'c']],
-      ['   a,    b   c', ['a', 'b', 'c']],
+      // Special case testing for allowed root-level directories.
+      // If the allowed root-level directory not correctly excluded - a
+      // different exception will be thrown.
+      [
+        [
+          '__MACOSX/',
+          'dir1/' . MinisiteAsset::INDEX_FILE,
+          'dir1/file.txt',
+        ],
+        ['html', 'ext'],
+        'Archive has invalid content: File dir1/file.txt has invalid extension.',
+      ],
     ];
   }
 
